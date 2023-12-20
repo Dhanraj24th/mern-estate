51d802cf5ad8ab6a522df26ed2cef7d2a4bd8c69
Dhanraj24th <dhanrajchavan2000@gmail.com>
2023-12-20 16:54:36 +0530

    "connection to mongodb"

--- api/index.js
+++ api/index.js
@@ -1,4 +1,12 @@
 import express from 'express';
+import dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
+mongoose.connect(process.env.MONGO).then(()=> {
+    console.log("connected to mongodb");
+}).catch((err)=>{
+    console.log(err);
+});
 const app=express();
 app.listen(3000,()=>{
     console.log("server has listening")
