323ab3ba202275765b54780817e13b597191df80
Dhanraj24th <dhanrajchavan2000@gmail.com>
2023-12-20 23:33:13 +0530

    custom error handled

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,17 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
-export const signup= async (req,res)=>{
+import { errorHandler } from "../utils/errorHandler.js";
+export const signup= async (req,res,next)=>{
   const {email,username,password}=req.body; 
   const hashedPassword=bcryptjs.hashSync(password,10);
   const newUser=new User({username,password:hashedPassword,email});
-  await newUser.save();
-     res.status(201).json({
-        message:"user created successfully",
-     });
+  try {
+    await newUser.save();
+    res.status(201).json({
+       message:"user created successfully",
+    });
+  } catch (error) {
+   next(errorHandler(500,"error from the function"));
+  }
+ 
 }
---
+++ api/utils/errorHandler.js
@@ -0,0 +1,6 @@
+export const errorHandler=(statusCode,Message)=>{
+  let newError=new Error();
+  newError.message=Message;
+  newError.statusCode=statusCode;
+  return newError;
+}
